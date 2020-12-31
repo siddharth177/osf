4a31f6b2fb25b8e12ad55e685bd6195153bbe0bd
raba-ajeet <raba.ajeet@gmail.com>
2020-12-31 16:19:01 +0530

    added repos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,36 @@
 const OSFText = "open source fest";
 const repos = [
     {
-        name: 'repo-name',
-        description: 'repo-description',
-        link: 'https://www.github.com/',
+        name: 'Hakons20',
+        description: 'This project includes the tracking details of the people travelling to their native places from different places. It keeps a digital record of the people. Problem Statement: Develop a web portal for COVID-19 with data database to help local authorities to find the people’s migration from one place to another place. The database is universally accessed by administrators to find the people in case of explosion corona cases.',
+        link: 'https://github.com/aadiiver/Hakons20',
     },
+    {
+        name: 'SoulSion',
+        description: 'Web Application for stress management and mental health wellness',
+        link: 'https://github.com/sauravshah31/SoulSion',
+    },
+    {
+        name: 'Text-to-handwritting',
+        description: 'We all get bored while writting our assignments so here i am working on a model which can make your assignments.',
+        link: 'https://github.com/ghanender-chauhan/Text-to-handwritting-OSF',
+    },
+    {
+        name: 'smolink-url_shortener',
+        description: 'This is a simple URL shortener in Flask. Shortens URL upto length 512 characters to 6 characters.',
+        link: 'https://github.com/ksh168/smolink-url_shortener',
+    },
+    {
+        name: 'blockchainvoting',
+        description: 'Voting using blockChain',
+        link: 'https://github.com/WebdevteamIG/blockchainvoting',
+    },
+    {
+        name: 'linkClock',
+        description: 'Basic idea of the app is to automate & manage the virtual meetings/classes before-hand. The user will store the URL related to the virtual meetings/classes along with the time. The app will fire an intent when the clock hits the time set by the User for that specific URL & will open it in browser. The user will be notified. So link to everything at one place will help manage multitasking',
+        link: 'https://github.com/SomalRudra/linkClock',
+    },
+
 ];
 const contributors = [
 
